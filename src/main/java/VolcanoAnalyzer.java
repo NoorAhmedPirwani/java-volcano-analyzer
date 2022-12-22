bb16efebe697db8e9f2518f3db6156b6db313b6c
NoorAhmedPirwani <noor.ahmad@xloopdigital.com>
2022-12-22 10:35:52 +0500

    test eruptions by country

--- src/main/java/VolcanoAnalyzer.java
+++ src/main/java/VolcanoAnalyzer.java
@@ -27,35 +27,41 @@ public class VolcanoAnalyzer {
         }
     }
 
+    
     public Integer numbVolcanoes(){
-
         return volcanos.size();
     }
 
+    //01. Return the volcanoes that erupted in the 1980s.
     public List<Volcano> eruptedInEighties(){
         return volcanos.stream().filter(eighties -> eighties.getYear() >= 1980 && eighties.getYear() < 1990).collect(Collectors.toList());
     }
 
+    //02. Return an array of the names of volcanoes that had an eruption with a Volcanic Explosivity Index (VEI) of 6 or higher.
     public List<String> highVEI(){
         return volcanos.stream().filter(high -> high.getVEI() >= 6).map(high->high.getName()).collect(Collectors.toList());
     }
 
+    //03. Return the eruption with the highest number of recorded deaths.
     // public List<Volcano> mostDeadly() {
-
     //     return volcanos.stream().sorted(Comparator.comparing((Volcano::getDEATHS))).limit(1).collect(Collectors.toList());
 
+    //04. Return the percentage of eruptions that caused tsunamis.
     public double causedTsunami() {
         return volcanos.stream().filter(tsuPercentage -> tsuPercentage.getTsu().equals("tsu")).count() * 100 / volcanos.size();
     }
 
-    public String mostCommonType() {     
-        
-        return volcanos.stream().collect(Collectors.collectingAndThen
-        (Collectors.groupingBy(Volcano::getType, Collectors.counting()),counted -> counted.entrySet()
-        .stream().max(Map.Entry.comparingByValue()).get().getKey()));        
-                        
+    //05. Return the most common type of volcano in the set.
+    public String mostCommonType() {       
+        return volcanos.stream().collect(Collectors.collectingAndThen(Collectors.groupingBy(Volcano::getType, Collectors.counting())
+        ,counted -> counted.entrySet().stream().max(Map.Entry.comparingByValue()).get().getKey()));
+     }
 
-     }    
+    //06. Return the number of eruptions when supplied a country as an argument.
+    public int eruptionsByCountry(String country) {
+        return volcanos.stream().filter(i -> i.getCountry().equals(country)).collect(Collectors.toList()).size();
+    }
+
     
     //add methods here to meet the requirements in README.md
 
